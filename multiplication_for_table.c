aa02e654badfce22ccb7ec67ecd59212b0a3c8a3
kenma-dev <kenma409@gmail.com>
2020-02-19 21:52:00 +0900

    multiple

---
+++ multiplication_for_table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int num = 0, i, result = 0;
+	printf("숫자를 입력하세요 : ");
+	scanf("%d", &num);
+	for (i = 1; i < 10; i = i + 2)
+	{
+		result = num * i;
+		printf("%d * %d = %d입니다. \n", num, i, result);
+	}
+	return 0;
+} 
