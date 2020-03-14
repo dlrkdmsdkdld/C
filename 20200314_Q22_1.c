8a0519e79c4303a69e1c279e2bc2dea48d24e7c1
kenma-dev <kenma409@gmail.com>
2020-03-14 23:08:50 +0900

    c lang Quiz

---
+++ 20200314_Q22_1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	struct employee
+	{
+		char name[10];
+		char number[20];
+		int salary;
+	};
+
+	struct employee arr[3] = {};
+	int i;
+	for (int i = 0; int < 3; i++);
+	
+
+}
