7cf8a06259bbc0f6d879540d0cf9b6630ff5a59e
kenma-dev <kenma409@gmail.com>
2020-03-19 20:36:29 +0900

    delete

--- 1010_12439569(AC).c
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-#define CRT_SECURE_NO_WARNINGS
-int main(void) 
-{
-	int a; 
-	scanf("%d",&a);
-	printf("%d",a);
-}
--- 1015_12439993(AC).c
+++
@@ -1,9 +0,0 @@
-﻿#include<stdio.h> 
-#define CRT_SECURE_NO_WARNINGS
-int main()
-{
-	float f; 
-	scanf("%f",&f);
-	printf("%.2f",f);
-	return 0; 
-}
